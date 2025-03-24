e5afa3d706b2ad463276010312ad5dbcefc98b0a
sofia-pastor <sofiapastor93@gmail.com>
2025-03-24 12:44:35 +0000

    started react app with vite

---
+++ meal-planner/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Meal Planner</h1>
+      </header>
+    </div>
+  );
+}
